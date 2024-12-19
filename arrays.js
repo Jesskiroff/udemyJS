3d048483f593cb552f692f5876c7ea7304a04a77
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-19 12:03:39 -0500

    add fizzbuzz

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var  guestList = ["Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+var guestName = prompt ("What is your name? ");
+guestList.includes(guestname)
--- leapYaer.js
+++ leapYaer.js
@@ -1,6 +1,6 @@
 function isLeap(year){
     if (year % 4 === 0) {
-        if(year % 100 == 0) {
+        if(year % 100 === 0) {
             if (year % 400 === 0) {
                 return"Leap year.";
             } else {
